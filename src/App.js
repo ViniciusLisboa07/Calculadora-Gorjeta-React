fb9a595b5a7123eb1be7df1d00d3cacdf4d06a6a
Vinicius Lisboa <vinicius.lisboa@quitandinha.pr.gov.br>
2021-05-27 17:00:10 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import './App.css';
 
-const Input =  styled.input`
+const Input = styled.input`
   width: 400px;
   height: 30px;
   font-size: 16px;
@@ -14,18 +14,31 @@ const Input =  styled.input`
 function App() {
   const [conta, setConta] = useState(0);
   const [porcentgem, setPorcentagem] = useState(0);
+
+  const handleInputConta = (e) => {
+    setConta(e.target.value);
+  };
+
+  const handleInputPorcentagem = (e) => {
+    setPorcentagem(e.target.value);
+  }
   
+  const gorjeta = useState(0);
+  const Total = useState(0);
+
   return (
     <div className="App">
       <div>
         <h1>Calculadora gorjeta!</h1>
         <p>Conta a pagar</p>
-        <Input place-holder="digite o valor da conta" type="number" value={conta}/>
+        <Input place-holder="digite o valor da conta" type="number" value={conta} onChange={handleInputConta} />
         <p>Porcentagem a pagar</p>
-        <Input place-holder="digite a porcentagem do garçom" type="number" value={porcentgem}/>
+        <Input place-holder="digite a porcentagem do garçom" type="number" value={porcentgem} onChange={handleInputPorcentagem} />
       </div>
       <div>
 
+        <p>Sub-Total: {conta}</p>
+        <p>Gorjeta({porcentgem}%): </p>
       </div>
     </div>
   );
