e90bd52dd27f1f87bfc7e078b140bf6110ab6584
Vinicius Lisboa <vinicius.lisboa@quitandinha.pr.gov.br>
2021-05-27 16:46:34 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
+const Input =  styled.input`
+  width: 400px;
+  height: 30px;
+  font-size: 16px;
+  padding: 10px;
+  border: 1px solid #000d; 
+`;
+
+
 function App() {
+  const [conta, setConta] = useState(0);
+  const [porcentgem, setPorcentagem] = useState(0);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Calculadora gorjeta!</h1>
+        <p>Conta a pagar</p>
+        <Input place-holder="digite o valor da conta" type="number" value={conta}/>
+        <p>Porcentagem a pagar</p>
+        <Input place-holder="digite a porcentagem do garçom" type="number" value={porcentgem}/>
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 }
